8eb12be47b620cd7ae0293f8b1a0e8eac01669b0
stephen18961 <stephen18961@gmail.com>
2024-08-16 09:21:52 +0700

    used popen and terminate instead of run

--- record.py
+++ record.py
@@ -15,22 +15,44 @@ def record_rtsp_stream(rtsp_url, output_dir, duration, max_retries=10, retry_int
     attempt = 0
     while attempt < max_retries:
         # FFmpeg command to capture the RTSP stream
+        # command = [
+        #     'ffmpeg',
+        #     '-rtsp_transport', 'tcp',  # Use TCP for better reliability
+        #     '-i', rtsp_url,             # Input RTSP URL
+        #     '-c:v', 'copy',             # Copy video codec
+        #     '-c:a', 'copy',             # Copy audio codec
+        #     output_file                 # Output file
+        # ]
+
         command = [
             'ffmpeg',
-            '-rtsp_transport', 'tcp',  # Use TCP for better reliability
-            '-i', rtsp_url,             # Input RTSP URL
-            '-c:v', 'copy',             # Copy video codec
-            '-c:a', 'copy',             # Copy audio codec
-            '-t', str(duration),        # Duration of the recording
-            output_file                 # Output file
+            '-hide_banner',
+            '-y',
+            '-loglevel', 'error',
+            '-use_wallclock_as_timestamps', '1',
+            '-rtsp_transport', 'tcp',
+            '-buffer_size', '1000000',
+            '-i', rtsp_url,
+            '-fps_mode', 'passthrough',
+            # '-c:v', 'libx265',
+            output_file
         ]
-
         try:
-            # Run FFmpeg command
-            subprocess.run(command, check=True)
+            # Run FFmpeg command with Popen
+            process = subprocess.Popen(command)
+
+            # Wait for the specified duration
+            time.sleep(duration)
+
+            # Terminate the process after the duration
+            process.terminate()
+
+            # Wait for the process to terminate
+            process.wait()
+
             print(f"Video saved to {output_file}")
             break  # Exit loop if successful
-        except subprocess.CalledProcessError as e:
+        except Exception as e:
             print(f"An error occurred: {e}")
             attempt += 1
             if attempt < max_retries:
